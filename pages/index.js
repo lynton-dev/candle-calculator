e278713a15ee378e498d8bf860e24eb25802a430
lynton-dev <61889203+lynton-dev@users.noreply.github.com>
2022-11-19 23:37:23 -0700

    Style updates
    And zero checking.

--- pages/index.js
+++ pages/index.js
@@ -12,10 +12,10 @@ export default function Home() {
   const [resultFragranceWeight, setResultFragranceWeight] = useState("0");
 
   function calculate() {
-    if (isNaN(vesselWeight) 
-    || isNaN(vesselPlusWaterWeight)
-    || isNaN(fragrance)
-    || isNaN(specificGravity)) {
+    if (isNaN(vesselWeight) || vesselWeight == 0
+    || isNaN(vesselPlusWaterWeight) || vesselPlusWaterWeight == 0
+    || isNaN(fragrance) || fragrance == 0
+    || isNaN(specificGravity) || specificGravity == 0) {
       return;
     }
     const waterWeight = vesselPlusWaterWeight - vesselWeight
@@ -130,7 +130,31 @@ export default function Home() {
 
         <div className="bottom-container">
           <div className="inline-form">
-            <button type="button" className="btn btn-primary" onClick={() => {calculate()}}> Calculate </button>
+            <button type="button" className="btn btn-primary" title="Calculate" onClick={() => {calculate()}}>
+              <svg
+                width="30"
+                height="30"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M17 5H7V7H17V5Z" fill="currentColor" />
+                <path d="M7 9H9V11H7V9Z" fill="currentColor" />
+                <path d="M9 13H7V15H9V13Z" fill="currentColor" />
+                <path d="M7 17H9V19H7V17Z" fill="currentColor" />
+                <path d="M13 9H11V11H13V9Z" fill="currentColor" />
+                <path d="M11 13H13V15H11V13Z" fill="currentColor" />
+                <path d="M13 17H11V19H13V17Z" fill="currentColor" />
+                <path d="M15 9H17V11H15V9Z" fill="currentColor" />
+                <path d="M17 13H15V19H17V13Z" fill="currentColor" />
+                <path
+                  fill-rule="evenodd"
+                  clip-rule="evenodd"
+                  d="M3 3C3 1.89543 3.89543 1 5 1H19C20.1046 1 21 1.89543 21 3V21C21 22.1046 20.1046 23 19 23H5C3.89543 23 3 22.1046 3 21V3ZM5 3H19V21H5V3Z"
+                  fill="currentColor"
+                />
+              </svg>
+            </button>
           </div>
         </div>
 
